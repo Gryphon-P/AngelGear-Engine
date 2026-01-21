aa69b6306768707458c21ecdf173dc7e538855ae
Gryphon Picciottoli <200626774@student.georgianc.on.ca>
2026-01-21 15:59:32 -0500

    Optimized render loop

--- main.cpp
+++ main.cpp
@@ -43,12 +43,67 @@ void check_shader_program_for_errors(unsigned int& program) {
     }
 }
 
-void render(GLFWwindow* window) {
+
+
+void render(GLFWwindow* window, unsigned int shader_program, unsigned int VAO) {
 
     glClearColor(0.f, 0.f, 0.f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    
+    
+    // Draw the triangle (to be moved to the main renderloop)
+    glUseProgram(shader_program);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
+
+
+
+
+
+
+
+
+
+
+}
+
+int main() {
+    
+
+    // Initializes GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+
+    // Creates a window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "rose_engine", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initializes GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // Sets viewport dimentions
+    glViewport(0, 0, 800, 600);
+    
+    // Resizes the viewport whenever the window changes size
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+#pragma region Rendering Init
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
@@ -73,7 +128,7 @@ void render(GLFWwindow* window) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Defines the vertex shader, stores the position data
-    const char* vertexShaderSource = 
+    const char* vertexShaderSource =
         "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
         "void main()\n"
@@ -163,58 +218,8 @@ void render(GLFWwindow* window) {
     // Set the vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+#pragma endregion
 
-
-    // Draw the triangle (to be moved to the main renderloop)
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-
-
-
-
-
-
-
-
-
-}
-
-int main() {
-    
-
-    // Initializes GLFW
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    
-
-    // Creates a window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "rose_engine", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    // Initializes GLAD
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-    // Sets viewport dimentions
-    glViewport(0, 0, 800, 600);
-    
-    // Resizes the viewport whenever the window changes size
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Main render loop
     while (!glfwWindowShouldClose(window))
@@ -226,7 +231,7 @@ int main() {
         // INPUT_END
 
         // RENDERING
-        render(window);
+        render(window, shaderProgram, VAO);
         // RENDERING_END
 
 
